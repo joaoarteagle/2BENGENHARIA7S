bef0974de3dabc2aaee3e725c45495fc9aec56c2
JoaoAndrade <joaopaulo.arteagle17@gmail.com>
2025-05-12 12:34:18 -0300

    first commit

---
+++ src/main/java/com/avajava/avaEliel/AvaElielApplication.java
@@ -0,0 +1,13 @@
+package com.avajava.avaEliel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaElielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaElielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avajava/avaEliel/AvaElielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avajava.avaEliel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaElielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
